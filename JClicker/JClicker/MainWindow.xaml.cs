6e87e6c8e20f33f9c133e3125ade53153dac184b
Jacob <65638832+jluvisi2021@users.noreply.github.com>
2020-06-14 17:23:25 -0700

    Implemented Pointer Function
    - Made pointer do its function.

--- JClicker/JClicker/MainWindow.xaml.cs
+++ JClicker/JClicker/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,6 +26,11 @@ namespace JClicker
     {
         private int TotalClicks = 0;
         private int TotalCoins = 0;
+        int Interval = 1000;
+        private double CountIntervalUpdates = 0;
+
+
+        Timer _timer;
 
         List<string> Upgrades = new List<string>();
 
@@ -31,19 +38,22 @@ namespace JClicker
         public MainWindow()
         {
             InitializeComponent();
-            
+          
+            // Start a repeating timer.
+            _timer = new Timer(Tick, null, Interval, Timeout.Infinite);
         }
+
 
         private void ClickButton_Click(object sender, RoutedEventArgs e)
         {
             TotalClicks++;
-            
-            if(TotalClicks % 100 == 0)
+            Console.WriteLine("User clicked the button!");
+            if(TotalClicks % 10 == 0)
             {
                 TotalCoins++;
                 
             }
-            update();
+            UpdateVisual();
 
         }
 
@@ -57,32 +67,65 @@ namespace JClicker
 
             TotalCoins--;
             AddUpgrade("Pointer");
-            update();
-
+           
         }
-
-
-
 
 
 
         public void AddUpgrade(String upgrade)
         {
             Upgrades.Add("Pointer");
+            UpdateVisual();
+
         }
-        private void update()
+
+        private void UpdateVisual()
         {
             ClickCounter.Content = "Total Clicks: " + TotalClicks;
             CoinCounter.Content = "Total Coins: " + TotalCoins;
-
-            for(int i = 0; i < Upgrades.Count; i++)
+            for (int i = 0; i < Upgrades.Count; i++)
             {
-                if(Upgrades[i]=="Pointer")
+                if (Upgrades[i] == "Pointer")
                 {
-                    int _Pointers = int.Parse(PointersLabel.Content.ToString().Substring(0, 1));
-                    _Pointers++;
-                    PointersLabel.Content = _Pointers + PointersLabel.Content.ToString().Substring(1);
+
+                    PointersLabel.Content = (i+1) + PointersLabel.Content.ToString().Substring(1);
                 }
+            }
+        }
+
+        public int CountUpdate(String name)
+        {
+            int num = 0;
+            for (int i = 0; i < Upgrades.Count; i++)
+            {
+                if(Upgrades[i] == name)
+                {
+                    num++;
+                }
+            }
+            return num;
+        }
+
+        private void Tick(object state)
+        {
+            try
+            {
+               
+                ++CountIntervalUpdates;
+                if(CountIntervalUpdates % 2 == 0)
+                {
+                    // Run things after 2 seconds have passed.
+                    TotalClicks += CountUpdate("Pointer");
+                }
+                // Update visuals off main thread.
+                Dispatcher.Invoke(() =>
+                {
+                    UpdateVisual();
+                });
+            }
+            finally
+            {
+                _timer?.Change(Interval, Timeout.Infinite);
             }
         }
     }
